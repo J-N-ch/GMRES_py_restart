575277430161d1018486bfc4717fd12a9865e33e
Andrew Chen <andrewchen@lips-hci.com>
2020-05-08 15:16:44 +0800

    [desc] add initialization and run (draft)

--- GMRES.py
+++ GMRES.py
@@ -1,8 +1,28 @@
 import numpy as np
 
 class GMRES_API:
-    def __init__( self, A: np.array([], dtype = float ), b: np.array([], dtype = float ), max_iterations: int, threshold: float):
-        print("Hello")
+    def __init__( self,
+                  A: np.array([], dtype = float ),
+                  b: np.array([], dtype = float ),
+                  max_iterations: int,
+                  threshold: float ):
+
+        print("Hello GMRES")
+        self.A = A
+        self.b = b
+        self.max_iterations = max_iterations
+        self.threshold = threshold
+
+    def initial_guess_input( self, x: np.array([], dtype = float ) ):
+        print("Hello GMRES init input")
+        self.x = x
+        print("x = ", self.x)
+
+    def run( self ):
+        print("GMRES run!")
+
+        
+
     
 def main():
 
@@ -12,6 +32,13 @@ def main():
     b_mat = np.array( [1.00, 0.00] )
 
     GMRES_test_No1 = GMRES_API( A_mat, b_mat, 1, 0.01)
+
+    x_mat = np.array( [0.01, 0.01] )
+
+    GMRES_test_No1.initial_guess_input( x_mat )
+
+    GMRES_test_No1.run()
+
 
 if __name__ == '__main__':
     main()
