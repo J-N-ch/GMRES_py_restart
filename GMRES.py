ccaf158a0526596b456f4590b11eaa24e84de01c
Andrew Chen <andrewchen@lips-hci.com>
2020-05-08 14:40:49 +0800

    [desc] add the possible inputs for the GMRES class

--- GMRES.py
+++ GMRES.py
@@ -1,11 +1,17 @@
 import numpy as np
 
 class GMRES_API:
-    def __init__(self):
+    def __init__( self, A: np.array([], dtype = float ), b: np.array([], dtype = float ), max_iterations: int, threshold: float):
         print("Hello")
-
+    
 def main():
-    GMRES_test_No1 = GMRES_API
+
+    A_mat = np.array( [[0.01, 0.00],
+                       [0.00, 0.01]] )
+
+    b_mat = np.array( [1.00, 0.00] )
+
+    GMRES_test_No1 = GMRES_API( A_mat, b_mat, 1, 0.01)
 
 if __name__ == '__main__':
     main()
